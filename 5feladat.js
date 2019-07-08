7f4ee88f35b5eb468b270b786082dc78fadcad6c
Gyurcsik Dóra <gyurcsik.dora@gmail.com>
2019-07-08 09:31:41 +0200

    task5

---
+++ 5feladat.js
@@ -0,0 +1,19 @@
+// let a = 3;
+
+const switchFunc = (a) => {
+  switch (a) {
+    case 1:
+      console.log('egy');
+      break;
+    case 2:
+      console.log('kettő');
+      break;
+    case 3:
+      console.log('három');
+      break;
+    default:
+      console.log('más');
+  }
+};
+
+// switchFunc(a);
