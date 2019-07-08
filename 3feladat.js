37d5518fa34f1ce3c7ee209f45852c7b60cffefe
Gyurcsik Dóra <gyurcsik.dora@gmail.com>
2019-07-08 09:25:17 +0200

    task3

---
+++ 3feladat.js
@@ -0,0 +1,11 @@
+// let arr = [1, 2, 3, 4, 5];
+
+const sumArr = (arr) => {
+  let s = 0;
+  for (let i = 0; i < arr.length; i++) {
+    s = s + arr[i];
+  }
+  return s;
+};
+
+// console.log(sumArr(arr));
